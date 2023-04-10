4dc78acc90a843c790afab07e8880ff78732824e
brianKungu <bkungu07@gmail.com>
2023-04-10 15:49:35 +0300

    Aligned waitlist button to center

--- components/sections/Waitlist.js
+++ components/sections/Waitlist.js
@@ -7,7 +7,7 @@ export default function Waitlist() {
     <section id="waitlist" className="bg-darkPink">
       <div className="max-w-7xl mx-auto my-8 py-8 w-full px-4">
         <div className="md:flex flex-col block items-center justify-center md:gap-4 space-y-4">
-          <button className="bg-lightBlue hover:bg-darkBlue px-6 py-4 md:w-[300px] text-white rounded-md font-semibold">
+          <button className="bg-lightBlue hover:bg-darkBlue px-6 py-4 md:w-[300px] text-white rounded-md font-semibold w-full">
             <Link
               href="#"
               passHref
@@ -16,11 +16,11 @@ export default function Waitlist() {
               signup
             </Link>
           </button>
-          <div className="flex flex-col flex-1 gap-4">
+          <div className="flex flex-col flex-1 gap-4 text-center">
             <h2 className="font-bold text-3xl md:text-5xl text-white uppercase leading-8">
               Now to join the waitlist
             </h2>
-            <p className="text-center text-white/80 leading-8 text-lg">
+            <p className="text-white/80 leading-8 text-lg">
               Don&apos;t miss out on the chance to experience video shopping
             </p>
           </div>
