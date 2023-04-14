ed63048ed9cc3b7b8afebf39e6df6d6d547b9dde
brianKungu <bkungu07@gmail.com>
2023-04-14 08:52:57 +0300

    Replaced the required words

--- components/sections/Goodbye.js
+++ components/sections/Goodbye.js
@@ -4,21 +4,13 @@ import SectionComponent from "./SectionComponent";
 export default function Goodbye() {
   return (
     <SectionComponent className="flex flex-col items-center justify-center text-center">
-      <h2 className="font-bold text-3xl md:text-5xl text-darkBlue uppercase leading-8">
-        Say goodbye to the uncertainty of shopping on{" "}
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#405DE6] via-[#C13584] to-[#F56040]">
-          Instagram
-        </span>{" "}
-        and{" "}
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#ff0050] from-40% via-[#00f2ea] via-80% to-[#000000] t0-10%">
-          Tiktok
-        </span>
-        .
+      <h2 className="font-bold text-3xl md:text-5xl text-white uppercase leading-8">
+        #Online vs reality just got cancelled.
       </h2>
       <p className="mt-8 text-white/80 leading-8 md:text-xl text-xl">
         &quot;{" "}
-        <span className="uppercase font-bold">#Onliners reality just got cancelled</span>{" "}
-        <br /> Discover high quality videos with accurate and detailed product
+        
+        Discover high quality videos with accurate and detailed product
         descriptions.Basically, what you see is what you get. &quot;
       </p>
     </SectionComponent>
