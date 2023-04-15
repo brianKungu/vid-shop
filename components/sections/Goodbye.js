85d4d1f5bbde560cf4bd663ee9c69f3919db9322
brianKungu <bkungu07@gmail.com>
2023-04-15 13:21:08 +0300

    Added gradient to text

--- components/sections/Goodbye.js
+++ components/sections/Goodbye.js
@@ -6,9 +6,9 @@ export default function Goodbye() {
     <SectionComponent className="flex flex-col items-center justify-center text-center">
       <h2 className="font-bold text-3xl md:text-5xl text-white uppercase leading-8">
         #Onlinevsreality just got{" "}
-        <span className="bg-clip-text bg-gradient-to-r from-[#ff0050] from-40% via-[#00f2ea] via-80% to-[#000000] t0-10%">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#405DE6] via-[#C13584] to-[#F56040]">
           cancelled
-        </span>{" "}
+        </span>
       </h2>
       <p className="mt-8 text-white/80 leading-8 md:text-xl text-xl">
         Discover high quality videos with accurate and detailed product
